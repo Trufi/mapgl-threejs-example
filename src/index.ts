35b27a1ef2777989a2845eba83199b2a527b0ac8
Mstislav Zhivodkov <stevemyz@gmail.com>
2021-09-15 14:49:08 +0700

    Add model building

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,15 @@ import { mapPointFromLngLat } from '@trufi/utils/mapPoint/fromLngLat';
 
 const loader = new GLTFLoader();
 
-const lngLatcenter = [82.92494, 55.0294];
+const lngLatcenter = [82.92065129285754, 55.02554021395639];
 
-const map = new mapgl.Map('map', {
+const map = ((window as any).map = new mapgl.Map('map', {
     key: '042b5b75-f847-4f2a-b695-b5f58adc9dfd',
     center: lngLatcenter,
-    zoom: 16.8,
-});
+    zoom: 18,
+    pitch: 45,
+    rotation: 45,
+}));
 
 const camera = new THREE.Camera();
 camera.updateMatrixWorld = () => {};
@@ -39,54 +41,27 @@ directionalLight.position.set(0, 0, 1);
 const scene = new THREE.Scene();
 scene.add(light, directionalLight);
 
-const airplanes: Array<{ mesh: THREE.Object3D; direction: THREE.Vector3 }> = [];
+const model = new THREE.Object3D();
 
-for (let i = 0; i < 100; i++) {
-    airplanes.push(createAirplane());
-}
+loader.load('./asia_building/scene.gltf', (gltf) => {
+    model.add(gltf.scene);
+    model.rotateX(Math.PI / 2);
+    model.rotateY((8 * Math.PI) / 180);
+    const mapPointCenter = mapPointFromLngLat([82.9206210338287, 55.025532994612504]);
+    const k = 60; // about 50 meters
+    model.scale.set(k / 2, k, k);
+    model.position.set(mapPointCenter[0], mapPointCenter[1], 0);
+    scene.add(model);
+});
 
-function createAirplane() {
-    const angle = Math.random() * Math.PI * 2;
-    const mesh = new THREE.Object3D();
-    const k = 300; // about 50 meters
-    mesh.scale.set(k, k, k);
-    mesh.rotateY(Math.PI / 2);
-    mesh.rotateX(Math.PI / 2 - angle);
-    mesh.updateMatrix();
-    mesh.updateWorldMatrix(true, true);
-
-    const mapPointCenter = mapPointFromLngLat(lngLatcenter);
-    mesh.position.set(
-        mapPointCenter[0] + (Math.random() - 0.5) * 300000,
-        mapPointCenter[1] + (Math.random() - 0.5) * 300000,
-        20000 + Math.random() * 100000,
-    );
-    scene.add(mesh);
-
-    loader.load('./a5.glb', (gltf) => {
-        gltf.scene.rotateZ(Math.PI / 2);
-        mesh.add(gltf.scene);
-    });
-
-    const direction = new THREE.Vector3(Math.cos(angle), Math.sin(angle), 0);
-
-    return { mesh, direction };
-}
-
-let lastUpdateTime = Date.now();
 function loop() {
     requestAnimationFrame(loop);
-
-    const now = Date.now();
-    const dt = now - lastUpdateTime;
-
-    airplanes.forEach((airplane) => {
-        airplane.mesh.position.addScaledVector(airplane.direction, dt * 2);
-    });
-
     camera.matrixWorldInverse.fromArray(map.getProjectionMatrix());
+    if (map.getStyleZoom() < 17) {
+        scene.remove(model);
+    } else {
+        scene.add(model);
+    }
     renderer.render(scene, camera);
-
-    lastUpdateTime = now;
 }
 requestAnimationFrame(loop);
